d56a72c60f032a9f89776ae5e542464761193f43
Fagundes1993 <mfbl93@gmail.com>
2023-08-31 22:38:13 -0300

    Update das queries

---
+++ queries_ecommerce.sql
@@ -0,0 +1,32 @@
+select * from productSeller;
+
+select count(*) from clients;
+select * from clients c, orders o where c.idClient = idOrderClient;
+
+select Fname,Lname, idOrder, orderStatus from clients c, orders o where c.idClient = idOrderClient;
+select concat(Fname,' ',Lname) as Client, idOrder as Request, orderStatus as Status from clients c, orders o where c.idClient = idOrderClient;
+
+insert into orders (idOrderClient, orderStatus, orderDescription, sendValue, paymentCash) values 
+							 (2, default,'compra via aplicativo',null,1);
+                             
+select count(*) from clients c, orders o 
+			where c.idClient = idOrderClient;
+
+select * from orders;
+
+-- recuperação de pedido com produto associado
+select * from clients c 
+				inner join orders o ON c.idClient = o.idOrderClient
+                inner join productOrder p on p.idPOorder = o.idOrder
+		group by idClient; 
+        
+-- Recuperar quantos pedidos foram realizados pelos clientes?
+select c.idClient, Fname, count(*) as Number_of_orders from clients c 
+				inner join orders o ON c.idClient = o.idOrderClient
+		group by idClient;
+
+-- não há vendedor que seja fornecedor
+select * from supplier;
+select * from seller;
+
+select * from product;
